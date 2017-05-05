c91b3bc85324eccaf18454f11a01918e46658baf
Tejaswaroop Vankadari <tvankada@cisco.com>
2017-05-04 20:27:58 -0700

    Added cart apis

--- services/routes/analytics/user.js
+++ services/routes/analytics/user.js
@@ -38,8 +38,12 @@ User.post('/adduser', (req, res) => {
             connection.query("CALL AddUser(" + userParams + ")", function(err, results, fields) {
                 if (err) {
                     res.json({ error: "failToSaveUser"});
+                    connection.destroy();
+                    console.log("Database connection is safely closed ...");
                 } else {
                     res.json({ results });
+                    connection.destroy();
+                    console.log("Database connection is safely closed ...");
                 }
             });
         }
@@ -71,8 +75,12 @@ User.post('/edituser', (req, res) => {
                 if (err) {
                     console.log(err);
                     res.json({ error: "failToSaveUser"});
+                    connection.destroy();
+                    console.log("Database connection is safely closed ...");
                 } else {
                     res.json({ results });
+                    connection.destroy();
+                    console.log("Database connection is safely closed ...");
                 }
             });
         }
@@ -93,9 +101,13 @@ User.post('/getusers', (req, res) => {
             connection.query("CALL LoadAllUsers()", function(err, results, fields) {
                 if (err) {
                     console.log(err);
-                    res.json({ error: "failTogetFromServer"});
+                    res.json({ error: "failTogetDataFromServer"});
+                    connection.destroy();
+                    console.log("Database connection is safely closed ...");
                 } else {
                     res.json({ users: results[0] });
+                    connection.destroy();
+                    console.log("Database connection is safely closed ...");
                 }
             });
         }
@@ -122,8 +134,127 @@ User.post('/deleteuser', (req, res) => {
                 if (err) {
                     console.log(err);
                     res.json({ error: "failToSaveUser"});
+                    connection.destroy();
+                    console.log("Database connection is safely closed ...");
                 } else {
                     res.json({ results });
+                    connection.destroy();
+                    console.log("Database connection is safely closed ...");
+                }
+            });
+        }
+    });
+
+});
+
+/**
+ * Returns cart information as output
+ * * **/
+
+User.post('/cartinfo', (req, res) => {
+    const connection = connectToDB();
+    connection.connect((err) => {
+        if(!err) {
+            connection.query('USE snackbar');
+            console.log("Database is connected ...");
+            connection.query("CALL GetCartInfo()", function(err, results, fields) {
+                if (err) {
+                    console.log(err);
+                    res.json({ error: "failTogetDataFromServer"});
+                } else {
+                    res.json({ cartInfo: results[0] });
+                }
+            });
+        }
+    });
+
+});
+
+/**
+ * Returns order frequency information as output
+ * * **/
+
+User.post('/getOrderFrequency', (req, res) => {
+    const connection = connectToDB();
+    connection.connect((err) => {
+        if(!err) {
+            connection.query('USE snackbar');
+            console.log("Database is connected ...");
+            connection.query("CALL GetOrderFrequency()", function(err, results, fields) {
+                if (err) {
+                    console.log(err);
+                    res.json({ error: "failTogetDataFromServer"});
+                    connection.destroy();
+                    console.log("Database connection is safely closed ...");
+                } else {
+                    res.json({ orderFrequency: results[0] });
+                    connection.destroy();
+                    console.log("Database connection is safely closed ...");
+                }
+            });
+        }
+    });
+
+});
+
+/**
+ * Accepts orderFrequency information as input
+ * Input Type: Json
+ * {
+ *      "orderFrequency":INTEGER_TYPE,
+ * }
+ * * **/
+
+User.post('/updateOrderFrequency', (req, res) => {
+    const connection = connectToDB();
+    connection.connect((err) => {
+        if(!err) {
+            connection.query('USE snackbar');
+            let userParams = "'"+req.body.orderFrequency+"'";
+            console.log("Database is connected ...");
+            connection.query("CALL UpdateOrderFrequency(" + userParams + ")", function(err, results, fields) {
+                if (err) {
+                    console.log(err);
+                    res.json({ error: "failToSaveUser"});
+                    connection.destroy();
+                    console.log("Database connection is safely closed ...");
+                } else {
+                    res.json({ results });
+                    connection.destroy();
+                    console.log("Database connection is safely closed ...");
+                }
+            });
+        }
+    });
+
+});
+
+/**
+ * Accepts productId, quantity information as input
+ * Input Type: Json
+ * {
+ *      "productId":STRING,
+ *      "quantity":INTEGER
+ * }
+ * * **/
+
+User.post('/updateQuantity', (req, res) => {
+    const connection = connectToDB();
+    connection.connect((err) => {
+        if(!err) {
+            connection.query('USE snackbar');
+            let userParams = "'"+req.body.productId+"', '"+req.body.quantity+"'";
+            console.log("Database is connected ...");
+            connection.query("CALL UpdateQuantityForItem(" + userParams + ")", function(err, results, fields) {
+                if (err) {
+                    console.log(err);
+                    res.json({ error: "failToSaveUser"});
+                    connection.destroy();
+                    console.log("Database connection is safely closed ...");
+                } else {
+                    res.json({ results });
+                    connection.destroy();
+                    console.log("Database connection is safely closed ...");
                 }
             });
         }
